a3422465b0c764a882a04708fb85e3dafcc259eb
Sameed Ahmed <bastamasta3150@proton.me>
2026-07-11 04:33:05 +0300

    completed the opcode list!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::random;
+
 pub const SCREEN_WIDTH: usize = 64; // screen pixel width
 pub const SCREEN_HEIGHT: usize = 32; // screen pixel height
 
@@ -261,6 +263,153 @@ impl Emu {
                 let y = digit3 as usize;
                 if self.v_reg[x] != self.v_reg[y] {
                     self.pc += 2;
+                }
+            }
+            // I = NNN
+            (0xA, _, _, _) => {
+                let nnn = op & 0xFFF;
+                self.i_reg = nnn;
+            }
+            // JMP to V0 + NNN
+            (0xB, _, _, _) => {
+                let nnn = op & 0xFFF;
+                self.pc = (self.v_reg[0] as u16) + nnn;
+            }
+            // VX = rand() & nn;
+            (0xC, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (op & 0xFF) as u8;
+                let rng: u8 = random();
+                self.v_reg[x] = rng & nn;
+            }
+            // DRAW
+            (0xD, _, _, _) => {
+                // get oncreen xy coords
+                let x_coord = self.v_reg[digit2 as usize];
+                let y_coord = self.v_reg[digit3 as usize];
+                // last digit gives height of sprite
+                let num_rows = digit4;
+
+                // keeep track if any pixels were flipped
+                let mut flipped = false;
+                // iter over each sprite row
+                for y_line in 0..(num_rows as u8) {
+                    // find mem addr of row
+                    let addr = self.i_reg + y_line as u16;
+                    let pixels = self.ram[addr as usize];
+                    // iter over each columns
+                    for x_line in 0..8 {
+                        // use mask to fetch current pixels bit. only flip if its a 1
+                        if (pixels & (0b1000_0000 >> x_line)) != 0 {
+                            // sprite should wrap around screen, so modulo
+                            let x = (x_coord + x_line) as usize % SCREEN_WIDTH;
+                            let y = (y_coord + y_line) as usize % SCREEN_HEIGHT;
+
+                            // get pixel index for 1d screen array
+                            let idx = x + SCREEN_WIDTH * y;
+                            flipped |= self.screen[idx];
+                            self.screen[idx] ^= true;
+                        }
+                    }
+                }
+                // populate vf register if flipped
+                if flipped {
+                    self.v_reg[0xF] = 1;
+                } else {
+                    self.v_reg[0xF] = 0;
+                }
+            }
+            // SKIP ON KEY PRESS
+            (0xE, _, 9, 0xE) => {
+                let x = digit2 as usize;
+                let vx = self.v_reg[x];
+                let key = self.keys[vx as usize];
+                if key {
+                    self.pc += 2;
+                }
+            }
+            // SKIP IF KEY NOT PRESSED
+            (0xE, _, 0xA, 1) => {
+                let x = digit2 as usize;
+                let vx = self.v_reg[x];
+                let key = self.keys[vx as usize];
+                if !key {
+                    self.pc += 2;
+                }
+            }
+            // VX = DT
+            (0xF, _, 0, 7) => {
+                let x = digit2 as usize;
+                self.v_reg[x] = self.dt;
+            }
+            // WAIT FOR KEY PRESS
+            (0xF, _, 0, 0xA) => {
+                let x = digit2 as usize;
+                let mut pressed = false;
+                for i in 0..self.keys.len() {
+                    if self.keys[i] {
+                        self.v_reg[x] = i as u8;
+                        pressed = true;
+                        break;
+                    }
+                }
+                if !pressed {
+                    // if nothing pressed redo opcode
+                    self.pc -= 2;
+                }
+            }
+            // DT = VX
+            (0xF, _, 1, 5) => {
+                let x = digit2 as usize;
+                self.dt = self.v_reg[x];
+            }
+            // ST = VX
+            (0xF, _, 1, 8) => {
+                let x = digit2 as usize;
+                self.st = self.v_reg[x];
+            }
+            // I += VX
+            (0xF, _, 1, 0xE) => {
+                let x = digit2 as usize;
+                let vx = self.v_reg[x] as u16;
+                self.i_reg = self.i_reg.wrapping_add(vx);
+            }
+            // I = FONT
+            (0xF, _, 2, 9) => {
+                let x = digit2 as usize;
+                let c = self.v_reg[x] as u16;
+                self.i_reg = c * 5; // I stored font at the bginnign of ram, and each font is 5 bytes
+            }
+            // BCD -> represent teh complete number as sepaarte ones, tens hundreds digits
+            (0xF, _, 3, 3) => {
+                let x = digit2 as usize;
+                let vx = self.v_reg[x] as f32;
+
+                // fetch the hundreds difit by /100 and toss decimal
+                let hundreds = (vx / 100.0).floor() as u8;
+                // same for tens, but also discard hundreds digit by modulo 10
+                let tens = ((vx / 10.0) % 10.0).floor() as u8;
+                // ones just modulo 10
+                let ones = (vx % 10.0) as u8;
+
+                self.ram[self.i_reg as usize] = hundreds;
+                self.ram[(self.i_reg + 1) as usize] = tens;
+                self.ram[(self.i_reg + 2) as usize] = ones;
+            }
+            // STORE V0 - VX to RAM
+            (0xF, _, 5, 5) => {
+                let x = digit2 as usize;
+                let i = self.i_reg as usize;
+                for idx in 0..=x {
+                    self.ram[i + idx] = self.v_reg[idx];
+                }
+            }
+            // LOAD V0 - VX from RAM
+            (0xF, _, 6, 5) => {
+                let x = digit2 as usize;
+                let i = self.i_reg as usize;
+                for idx in 0..=x {
+                    self.v_reg[x] = self.ram[i + idx];
                 }
             }
             (_, _, _, _) => unimplemented!("unimplemented opcode: {}", op),
