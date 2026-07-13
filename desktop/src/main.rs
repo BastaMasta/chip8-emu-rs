9cdbfb223ca0c1e71a1e34440e0ea51e5acda5d0
Sameed Ahmed <bastamasta3150@proton.me>
2026-07-13 04:35:22 +0300

    fucking fixed it, just needed to use the bundled feature of the sdl2 crate. might update to sdl3 later if i feel comfortable. looks like ill learn winit and pixels later.

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,17 +1,8 @@
 use chip8_core::*;
+use sdl2::event::Event;
 use std::env;
 
-use std::sync::Arc;
-
-use pixels::{Error, Pixels, SurfaceTexture};
-use winit::dpi::LogicalSize;
-use winit::event::{self, Event, WindowEvent};
-use winit::event_loop::{self, EventLoop};
-use winit::keyboard::KeyCode;
-use winit::window::Window;
-use winit_input_helper::WinitInputHelper;
-
-const SCALE: u32 = 15;
+const SCALE: u32 = 10;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 
@@ -22,5 +13,28 @@ fn main() {
         return;
     }
 
-    // Setup pixels
+    // Setup SDL
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Chip-8 Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'gameloop: loop {
+        for evt in event_pump.poll_iter() {
+            match evt {
+                Event::Quit { .. } => {
+                    break 'gameloop;
+                }
+                _ => (),
+            }
+        }
+    }
 }
