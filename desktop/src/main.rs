60a310c4d330cfa1026e951d4a146de7a94d397c
Sameed Ahmed <bastamasta3150@proton.me>
2026-07-13 17:23:48 +0300

    fixed bug - start_addr was set to 200 instead of 0x200. caused program to crash. also loaded bilerplate and started the emultor anf loaded a game successfully

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,6 +1,12 @@
 use chip8_core::*;
 use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
 const SCALE: u32 = 10;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
@@ -27,14 +33,45 @@ fn main() {
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut chip8 = Emu::new();
+    let mut rom = File::open(&args[1]).expect("unable to open file");
+    let mut buffer = Vec::new();
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load(&buffer);
+
     'gameloop: loop {
-        for evt in event_pump.poll_iter() {
-            match evt {
+        for event in event_pump.poll_iter() {
+            match event {
                 Event::Quit { .. } => {
                     break 'gameloop;
                 }
                 _ => (),
             }
         }
+        chip8.tick();
+        draw_screen(&chip8, &mut canvas);
     }
+}
+
+fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
+    // cls on canvas to blck
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let screen_buf = emu.get_display();
+    // now set col to white, the iter and check if pixel is to be drawn to
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    for (i, pixel) in screen_buf.iter().enumerate() {
+        if *pixel {
+            // convert 1d array to 2d pos
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+
+            // draw a rect at x,y, scaled up by scale value
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
+    canvas.present();
 }
